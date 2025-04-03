d6220f3c98edfd009cebc33c67d8d53b268a87d8
MrMagmaPT <pmrmagma@gmail.com>
2025-04-03 14:21:57 +0100

    Criei a base para a ligação entre cliente e servidor

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.btnEnviarMsg = new System.Windows.Forms.Button();
+            this.tbxMsg = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnEnviarMsg
+            // 
+            this.btnEnviarMsg.Location = new System.Drawing.Point(609, 52);
+            this.btnEnviarMsg.Name = "btnEnviarMsg";
+            this.btnEnviarMsg.Size = new System.Drawing.Size(154, 91);
+            this.btnEnviarMsg.TabIndex = 0;
+            this.btnEnviarMsg.Text = "Enviar Mensagem";
+            this.btnEnviarMsg.UseVisualStyleBackColor = true;
+            this.btnEnviarMsg.Click += new System.EventHandler(this.btnEnviarMsg_Click);
+            // 
+            // tbxMsg
+            // 
+            this.tbxMsg.Location = new System.Drawing.Point(49, 52);
+            this.tbxMsg.Multiline = true;
+            this.tbxMsg.Name = "tbxMsg";
+            this.tbxMsg.Size = new System.Drawing.Size(539, 368);
+            this.tbxMsg.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbxMsg);
+            this.Controls.Add(this.btnEnviarMsg);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button btnEnviarMsg;
+        private System.Windows.Forms.TextBox tbxMsg;
     }
 }
 
